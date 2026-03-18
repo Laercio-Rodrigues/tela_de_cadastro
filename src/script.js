08d211889ca3dc6e3eb9203b3071f3408c9f7b4a
Laércio-Rodrigues <laerrcioo91@gmail.com>
2026-03-18 12:44:21 -0400

    remocao dos console.log para adicionar o array no ./src/script.js

--- src/script.js
+++ src/script.js
@@ -10,10 +10,7 @@ const cadastrar = () => {
     const inputConfirmPassword = document.querySelector('.input-confirmar-senha'). value
 
     // Sua tarefa é colocar essas informaçoes dentro de um array
-    console.log(inputName)
-    console.log(inputEmail)
-    console.log(inputPassword)
-    console.log(inputConfirmPassword)
+
 }
 
 button.addEventListener('click', cadastrar)
